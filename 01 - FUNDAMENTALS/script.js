c53817a2b877dfd92f8fbc8b2302a5ee5c251a97
Ability Ekpat <abilityekpat@gmail.com>
2025-03-26 16:06:44 +0100

    javascript Datatype

---
+++ 01 - FUNDAMENTALS/script.js
@@ -0,0 +1,15 @@
+let js = "Amazing";
+console.log(3+4+3+6);
+
+
+let JavaScriptIsFun =true;
+console.log(JavaScriptIsFun);
+console.log(typeof true);
+
+
+JavaScriptIsFun = "Yes!";
+console.log(JavaScriptIsFun);
+console.log(typeof "Yes!");
+
+
+
