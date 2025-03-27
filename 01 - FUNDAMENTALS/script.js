f8f1e8a38d0682eafb219ed51b21e60fceb70510
Ability Ekpat <abilityekpat@gmail.com>
2025-03-27 19:02:20 +0100

    basic operation

--- 01 - FUNDAMENTALS/script.js
+++ 01 - FUNDAMENTALS/script.js
@@ -1,15 +1,66 @@
-let js = "Amazing";
-console.log(3+4+3+6);
+////////////////////////////////////
+// Linking a JavaScript File
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+////////////////////////////////////
+// Values and Variables
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
 
 
-let JavaScriptIsFun =true;
+// Variable name conventions
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
+
+
+
+// Data Types
+
+let JavaScriptIsFun = true;
 console.log(JavaScriptIsFun);
-console.log(typeof true);
-
+console.log(typeof JavaScriptIsFun)
 
 JavaScriptIsFun = "Yes!";
 console.log(JavaScriptIsFun);
-console.log(typeof "Yes!");
+console.log(typeof JavaScriptIsFun);
+
+
+let name = "Grace";
+console.log(name);
+console.log(typeof name);
+
+//Variable declaration let,const and var
+
+let age = 3;
+age = 34;
+console.log(age);
+
+const year =1999;
+console.log(year)
+
+//Basic Operators
+const ageJonas = 2034 - 1991
+const ageSarah = 3023 - 1998
+console.log(ageJonas, ageSarah)
+
 
 
 
