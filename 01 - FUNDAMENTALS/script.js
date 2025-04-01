fc95faf504f45e73cb4f49d1b1d792534b38e004
Ability Ekpat <abilityekpat@gmail.com>
2025-04-01 02:35:40 +0100

    if else statement

--- 01 - FUNDAMENTALS/script.js
+++ 01 - FUNDAMENTALS/script.js
@@ -1,65 +1,91 @@
 ////////////////////////////////////
 // Linking a JavaScript File
-let js = "amazing";
-console.log(40 + 8 + 23 - 10);
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
 
 ////////////////////////////////////
 // Values and Variables
-console.log("Jonas");
-console.log(23);
+////////////////////////////////////
+// console.log("Jonas");
+// console.log(23);
 
-let firstName = "Matilda";
+// let firstName = "Matilda";
 
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
 
 
 // Variable name conventions
-let jonas_matilda = "JM";
-let $function = 27;
+////////////////////////////////////
+// let jonas_matilda = "JM";
+// let $function = 27;
 
-let person = "jonas";
-let PI = 3.1415;
+// let person = "jonas";
+// let PI = 3.1415;
 
-let myFirstJob = "Coder";
-let myCurrentJob = "Teacher";
+// let myFirstJob = "Coder";
+// let myCurrentJob = "Teacher";
 
-let job1 = "programmer";
-let job2 = "teacher";
+// let job1 = "programmer";
+// let job2 = "teacher";
 
-console.log(myFirstJob);
+// console.log(myFirstJob);
 
 
 
 // Data Types
+////////////////////////////////////
 
-let JavaScriptIsFun = true;
-console.log(JavaScriptIsFun);
-console.log(typeof JavaScriptIsFun)
+// let JavaScriptIsFun = true;
+// console.log(JavaScriptIsFun);
+// console.log(typeof JavaScriptIsFun)
 
-JavaScriptIsFun = "Yes!";
-console.log(JavaScriptIsFun);
-console.log(typeof JavaScriptIsFun);
+// JavaScriptIsFun = "Yes!";
+// console.log(JavaScriptIsFun);
+// console.log(typeof JavaScriptIsFun);
 
 
-let name = "Grace";
-console.log(name);
-console.log(typeof name);
+// let name = "Grace";
+// console.log(name);
+// console.log(typeof name);
 
 //Variable declaration let,const and var
 
-let age = 3;
-age = 34;
-console.log(age);
+// let age = 3;
+// age = 34;
+// console.log(age);
 
-const year =1999;
-console.log(year)
+// const year =1999;
+// console.log(year)
 
 //Basic Operators
 const ageJonas = 2034 - 1991
 const ageSarah = 3023 - 1998
 console.log(ageJonas, ageSarah)
+
+// Assignment operators
+let x = 15;
+x = x + 5;
+x += 10 // x = x + 10;
+x *= 4 // x = x * 4;
+x++; // x = x + 1;
+x--; // x = x - 1;
+console.log(x)
+
+//comparison operators
+console.log(ageJonas > ageSarah);
+console.log(ageJonas >= 30);
+
+const age = 15;
+if(age >=18){
+console.log("Sarah can have a driving License")
+}else{
+    const ageLeft = 18 - age;
+    console.log(`Sarah is too young to drivr, wait for another ${ageLeft} years`)
+
+}
+
 
 
 
